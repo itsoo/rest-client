0b6bc82529630aa03db40759aba9b043e4d42a27
zxy <84235789@qq.com>
2020-11-26 17:50:10 +0800

    .

--- src/main/java/com/cupshe/restclient/AnnotationMethodAttribute.java
+++ src/main/java/com/cupshe/restclient/AnnotationMethodAttribute.java
@@ -1,8 +1,8 @@
 package com.cupshe.restclient;
 
 import com.cupshe.restclient.exception.NoSupportMethodException;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpMethod;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.annotation.Annotation;
@@ -31,16 +31,31 @@ class AnnotationMethodAttribute {
         this.method = method;
     }
 
-    static AnnotationMethodAttribute of(Method method) {
-        return of(method.getDeclaredAnnotations()[0]);
-    }
-
     boolean isPassingParamsOfUrl() {
         return GET.equals(method) || DELETE.equals(method);
     }
 
     boolean isPassingParamsOfForm() {
         return POST.equals(method) || PUT.equals(method) || PATCH.equals(method);
+    }
+
+    static AnnotationMethodAttribute of(Method method) {
+        Annotation annotation;
+        if ((annotation = findAnnotation(method, GetMapping.class)) != null) {
+            return of(annotation);
+        } else if ((annotation = findAnnotation(method, PostMapping.class)) != null) {
+            return of(annotation);
+        } else if ((annotation = findAnnotation(method, PutMapping.class)) != null) {
+            return of(annotation);
+        } else if ((annotation = findAnnotation(method, PatchMapping.class)) != null) {
+            return of(annotation);
+        } else if ((annotation = findAnnotation(method, DeleteMapping.class)) != null) {
+            return of(annotation);
+        } else if ((annotation = findAnnotation(method, RequestMapping.class)) != null) {
+            return of(annotation);
+        } else {
+            throw new NoSupportMethodException();
+        }
     }
 
     static AnnotationMethodAttribute of(Annotation annotation) {
@@ -62,35 +77,34 @@ class AnnotationMethodAttribute {
     }
 
     private static AnnotationMethodAttribute of(GetMapping t) {
-        return of(getOrDefault(t.path(), t.value()), t.headers(), t.params(), GET);
+        return of(t.path(), t.headers(), t.params(), GET);
     }
 
     private static AnnotationMethodAttribute of(PostMapping t) {
-        return of(getOrDefault(t.path(), t.value()), t.headers(), t.params(), POST);
+        return of(t.path(), t.headers(), t.params(), POST);
     }
 
     private static AnnotationMethodAttribute of(PutMapping t) {
-        return of(getOrDefault(t.path(), t.value()), t.headers(), t.params(), PUT);
+        return of(t.path(), t.headers(), t.params(), PUT);
     }
 
     private static AnnotationMethodAttribute of(PatchMapping t) {
-        return of(getOrDefault(t.path(), t.value()), t.headers(), t.params(), PATCH);
+        return of(t.path(), t.headers(), t.params(), PATCH);
     }
 
     private static AnnotationMethodAttribute of(DeleteMapping t) {
-        return of(getOrDefault(t.path(), t.value()), t.headers(), t.params(), DELETE);
+        return of(t.path(), t.headers(), t.params(), DELETE);
     }
 
     private static AnnotationMethodAttribute of(RequestMapping t) {
-        return of(getOrDefault(t.path(), t.value()), t.headers(), t.params(), resolve(t.method()[0].name()));
+        return of(t.path(), t.headers(), t.params(), resolve(t.method()[0].name()));
     }
 
     private static AnnotationMethodAttribute of(String[] paths, String[] headers, String[] params, HttpMethod method) {
         return new AnnotationMethodAttribute(paths, headers, params, method);
     }
 
-    @NonNull
-    private static String[] getOrDefault(String[] arg, String[] def) {
-        return arg.length == 0 ? def : arg;
+    private static <A extends Annotation> A findAnnotation(Method method, Class<A> annotationType) {
+        return AnnotationUtils.findAnnotation(method, annotationType);
     }
 }
