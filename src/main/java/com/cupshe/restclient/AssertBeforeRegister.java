a22fe03a5c728d44793c34f288e6b85a1d52e209
zxy <84235789@qq.com>
2020-12-03 09:24:16 +0800

    .

--- src/main/java/com/cupshe/restclient/AssertBeforeRegister.java
+++ src/main/java/com/cupshe/restclient/AssertBeforeRegister.java
@@ -16,8 +16,8 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 /**
  * AssertBeforeRegister
@@ -27,7 +27,7 @@ import java.util.concurrent.ConcurrentMap;
 @PureFunction
 class AssertBeforeRegister {
 
-    private static ConcurrentMap<String, String> ALL_REGISTERED_BEANS = new ConcurrentHashMap<>();
+    private static Map<String, String> ALL_REGISTERED_BEANS = new ConcurrentHashMap<>(32);
 
     static void assertSingletonRegister(String beanName, String className) {
         String repClassName = ALL_REGISTERED_BEANS.get(beanName);
